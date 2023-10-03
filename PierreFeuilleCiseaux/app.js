10f2ba12be4bb5323329040d84fbf6d473ef174b
Maximeprime93200 <m_aiad@etu-webschoolfactory.fr>
2023-10-03 22:23:37 +0200

    Test

--- PierreFeuilleCiseaux/app.js
+++ PierreFeuilleCiseaux/app.js
@@ -1,26 +1,26 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const app = express();
 
-mongoose.connect('mongodb://localhost/votre-base-de-donnees', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erreur de connexion à la base de données :'));
-db.once('open', () => {
-  console.log('Connecté à la base de données');
-});
+// Middleware pour parser les requêtes JSON
+app.use(bodyParser.json());
 
-app.use(express.json());
-
-const jeuRoutes = require('./routes/jeu');
-const utilisateurRoutes = require('./routes/utilisateur');
-
-app.use('/jeu', jeuRoutes);
-app.use('/utilisateur', utilisateurRoutes);
+// Configuration d'express-session avec session-file-store
+app.use(session({
+  secret: 'votre_secret_session', // Clé secrète pour les sessions
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore({
+    path: './sessions', // Répertoire où les sessions seront stockées (créez ce dossier)
+  }),
+}));
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`Serveur en écoute sur le port ${port}`);
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
 });
