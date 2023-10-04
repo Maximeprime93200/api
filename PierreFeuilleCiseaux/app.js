71ba9f64f756906b9dc557972e95d53ffad0f97e
Maxime <m_aiad@etu-webschoolfactory.fr>
2023-10-05 00:15:28 +0200

    coups

--- PierreFeuilleCiseaux/app.js
+++ PierreFeuilleCiseaux/app.js
@@ -88,7 +88,8 @@ app.post('/jeu/creer-partie', (req, res) => {
   const nouvellePartie = {
     id: uuid.v4(),
     createur: userId, // L'ID de l'utilisateur qui crée la partie
-    joueurs: []
+    joueurs: [],
+    coups: {}
     // Autres informations sur la partie, comme les joueurs, le statut, etc.
   };
 
@@ -135,6 +136,66 @@ app.post('/jeu/rejoindre-partie/:partieId', (req, res) => {
   res.json({ message: 'Vous avez rejoint la partie avec succès.', partie });
 });
 
+// Endpoint pour que les joueurs effectuent un coup
+app.post('/jeu/faire-coup/:partieId', (req, res) => {
+  const { partieId } = req.params;
+  const userId = req.session.userId;
+  console.log(userId + "iddddddddddddd");
+  const { coup } = req.body;
+
+  // Chargez les données depuis le fichier db.json
+  const dbData = JSON.parse(fs.readFileSync('db.json'));
+
+  // Recherchez la partie par son ID
+  const partie = dbData.parties.find((p) => p.id === partieId);
+
+  if (!partie) {
+    return res.status(404).json({ message: 'Partie non trouvée.' });
+  }
+
+  // Vérifiez si l'utilisateur appartient à la partie
+  if (!partie.joueurs.includes(userId)) {
+    return res.status(401).json({ message: 'Vous ne faites pas partie de cette partie.' });
+  }
+
+  // Vérifiez si l'utilisateur a déjà effectué un coup
+  if (partie.coups[userId] !== "") {
+    return res.status(400).json({ message: 'Vous avez déjà effectué un coup dans cette partie.' });
+  }
+
+  // Mettez à jour la partie avec le coup du joueur
+  partie.coups[userId] = coup;
+
+  // Enregistrez les modifications dans le fichier db.json
+  fs.writeFileSync('db.json', JSON.stringify(dbData, null, 2));
+
+  // Vérifiez si les deux joueurs ont fait un coup
+  if (partie.coups[partie.joueurs[0]] && partie.coups[partie.joueurs[1]]) {
+    // Utilisez la fonction determineWinner pour déterminer le gagnant
+    const gagnant = determineWinner(partie.coups);
+    res.json({ message: 'Coup enregistré avec succès.', gagnant });
+  } else {
+    // Si un seul coup a été fait, indiquez d'attendre le coup de l'adversaire
+    res.json({ message: 'Coup enregistré avec succès. Attendez le coup de l\'adversaire.' });
+  }
+});
+
+function determineWinner(coups) {
+  const joueur1Coup = coups.joueur1Coup.toLowerCase();
+  const joueur2Coup = coups.joueur2Coup.toLowerCase();
+
+  if (joueur1Coup === joueur2Coup) {
+    return "match nul";
+  } else if (
+    (joueur1Coup === "pierre" && joueur2Coup === "ciseaux") ||
+    (joueur1Coup === "ciseaux" && joueur2Coup === "papier") ||
+    (joueur1Coup === "papier" && joueur2Coup === "pierre")
+  ) {
+    return "joueur1";
+  } else {
+    return "joueur2";
+  }
+}
 
 const port = process.env.PORT || 3000;
 
