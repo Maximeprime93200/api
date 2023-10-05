e83082b82a263e5cf9f9a76af5e509e87398aa3d
ThomassKPP <thomaskidney1@gmail.com>
2023-10-06 00:51:46 +0200

    Deconnexion, Historique, Statut, Plusieurs Parties

--- PierreFeuilleCiseaux/routes/jeu.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const jeuController = require('../controllers/jeuController');
-
-router.post('/creer-partie', jeuController.creerPartie);
-router.post('/rejoindre-partie', jeuController.rejoindrePartie);
-router.post('/jouer-tour', jeuController.jouerTour);
-
-module.exports = router;
